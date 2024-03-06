a8d7cb1092ed1e43b2816a5a21206c0f21e870be
Rakie j <karishjontez@gmail.com>
2024-03-06 16:39:49 +0300

    task due 06.03.24

--- Tasks.js
+++ Tasks.js
@@ -4,39 +4,41 @@ function calculateDistance(x, y) {
 }
 
 
-document.getElementById("coordinates").addEventListener("submit", function(event) {
+document.getElementById("pointForm").addEventListener("submit", function(event) {
     event.preventDefault(); 
 
     
-    var x1 = parseFloat(document.getElementById("x1").value);
-    var y1 = parseFloat(document.getElementById("y1").value);
-    var x2 = parseFloat(document.getElementById("x2").value);
-    var y2 = parseFloat(document.getElementById("y2").value);
-    var x3 = parseFloat(document.getElementById("x3").value);
-    var y3 = parseFloat(document.getElementById("y3").value);
-    var x4 = parseFloat(document.getElementById("x4").value);
-    var y4 = parseFloat(document.getElementById("y4").value);
-
-    var points = [
-        { name: 'A', x: x1, y: y1 },
-        { name: 'B', x: x2, y: y2 },
-        { name: 'C', x: x3, y: y3 },
-        { name: 'D', x: x4, y: y4 }
-    ];
-
-    points.forEach(point => {
-        point.distance = calculateDistance(point.x, point.y);
-    });
+    var xValues = [];
+    var yValues = [];
+    for (var i = 1; i <= 4; i++) {
+        xValues.push(parseFloat(document.getElementById("x" + i).value));
+        yValues.push(parseFloat(document.getElementById("y" + i).value));
+    }
 
     
-    points.sort((a, b) => b.distance - a.distance);
+    var points = [];
+    for (var i = 0; i < 4; i++) {
+        var distance = calculateDistance(xValues[i], yValues[i]);
+        points.push({ name: String.fromCharCode(65 + i), x: xValues[i], y: yValues[i], distance: distance });
+    }
+
+    
+    for (var i = 0; i < points.length - 1; i++) {
+        for (var j = 0; j < points.length - i - 1; j++) {
+            if (points[j].distance < points[j + 1].distance) {
+             
+                var temp = points[j];
+                points[j] = points[j + 1];
+                points[j + 1] = temp;
+            }
+        }
+    }
 
     
     var tableBody = document.getElementById("pointsBody");
     tableBody.innerHTML = "";
-    points.forEach(point => {
-        var row = "<tr><td>" + point.name + "</td><td>" + point.x + "</td><td>" + point.y + "</td><td>" + point.distance.toFixed(2) + "</td></tr>";
+    for (var i = 0; i < points.length; i++) {
+        var row = "<tr><td>" + points[i].name + "</td><td>" + points[i].x + "</td><td>" + points[i].y + "</td><td>" + points[i].distance.toFixed(2) + "</td></tr>";
         tableBody.innerHTML += row;
-    });
+    }
 });
-
