991eb30c81a94ea8aa6e7a95936c59b0dd45ae6d
Rakie j <karishjontez@gmail.com>
2024-04-29 08:50:54 +0300

    rectified matrix

---
+++ website/Task4.js
@@ -0,0 +1,84 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+  const table = document.getElementById("rowcolumns");
+  const btn = document.getElementById("button");
+
+  // Function to generate random numbers for a row
+  function generateRandomNumbers(columns) {
+    const rowRandomNumbers = [];
+    console.log(columns);
+    for (let j = 0; j < columns; j++) {
+      const randomNumber = Math.floor(Math.random() * 100);
+      rowRandomNumbers.push(randomNumber);
+    }
+    return rowRandomNumbers;
+  }
+
+  // Function to validate rows and columns
+  function validateInput(rows, columns) {
+    if (isNaN(rows) || rows <= 0 || isNaN(columns) || columns <= 0) {
+      alert("Please enter valid numbers of rows and columns");
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  // Function to find the largest number on the diagonal
+  function findLargestDiagonal(rows, allRandomNumbers) {
+    let largest = allRandomNumbers[0][rows - 1];
+    for (let i = 0; i < rows; i++) {
+      for (let j = rows - 1 - i; j < allRandomNumbers[i].length - i; j++) {
+        if (largest < allRandomNumbers[i][j]) {
+          largest = allRandomNumbers[i][j];
+        }
+      }
+    }
+    return largest;
+  }
+
+  // Function to generate table rows and columns
+  function generateTable(rows, columns, allRandomNumbers) {
+    table.innerHTML = ''; // Clear the table content
+    for (let i = 0; i < rows; i++) {
+      const newRow = table.insertRow();
+      for (let j = 0; j < columns; j++) {
+        const cell = newRow.insertCell();
+        const randomNumber = allRandomNumbers[i][j];
+        cell.innerHTML = `<input type='number' value=${randomNumber}>`;
+      }
+    }
+  }
+
+  // Event listener for button click
+  btn.addEventListener("click", () => {
+    const columns = parseFloat(document.getElementById("columns").value);
+    const rows = parseFloat(document.getElementById("rows").value);
+
+    if (!validateInput(rows, columns)) return;
+
+    const allRandomNumbers = [];
+
+    for (let i = 0; i < rows; i++) {
+      const rowRandomNumbers = generateRandomNumbers(columns);
+      allRandomNumbers.push(rowRandomNumbers);
+    }
+
+    generateTable(rows, columns, allRandomNumbers);
+
+    const largest = findLargestDiagonal(rows, allRandomNumbers);
+    console.log(largest);
+
+    document.getElementById("largest").value = largest;
+  });
+
+  // Function to reset table
+  document.getElementById("resetTable").addEventListener("click", resetTable);
+
+  function resetTable() {
+    table.innerHTML = ""; // Clear the table content
+    document.getElementById("rows").value= "";
+    document.getElementById("columns").value= "";
+    document.getElementById("largest").value = "";
+  }
+});
