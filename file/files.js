25ac8bbbb22fd4b02e709f9e9dddedc6438cca59
Rakie j <karishjontez@gmail.com>
2024-05-09 17:48:16 +0300

    file

--- file/files.js
+++ file/files.js
@@ -1,39 +1,39 @@
- //browsing from the user files
- let fileInput = document.getElementById("fileInput");
- let downloadedwords = document.getElementById("downloadedwords");
- const results=document.getElementById("results")
- const filteredArr = [];
- fileInput.addEventListener("change", (event) => {
-     const file = event.target.files[0];
-     const reader = new FileReader();
+//  //browsing from the user files
+//  let fileInput = document.getElementById("fileInput");
+//  let downloadedwords = document.getElementById("downloadedwords");
+//  const results=document.getElementById("results")
+//  const filteredArr = [];
+//  fileInput.addEventListener("change", (event) => {
+//      const file = event.target.files[0];
+//      const reader = new FileReader();
      
-     reader.addEventListener("load", (event) => {
-         const filecontents = event.target.result.split(" ");
-         if (filecontents.some((word) => word.includes("b"))) {
-             const filteredwords = filecontents.filter((word) => word.includes("b"));
+//      reader.addEventListener("load", (event) => {
+//          const filecontents = event.target.result.split(" ");
+//          if (filecontents.some((word) => word.includes("b"))) {
+//              const filteredwords = filecontents.filter((word) => word.includes("b"));
 
-             filteredArr.push(...filteredwords);
-             console.log(filteredArr);
-             let longestWord = "";
-             for (const word of filteredArr) {
-                 if (word.length > longestWord.length) {
-                     longestWord = word;
-                     results.innerHTML=`<strong>The longest word with letter 'b':</strong>  <mark> ${longestWord}</mark> `;
-                 }
-             }
-             // blob for browsed file
-             const blob = new Blob([filteredArr.join("\n")], { type: "text/plain" });
-             console.log(blob);
-             const fileDownload = new File([blob], "words.txt", { type: "text/plain" });
-             link.href = URL.createObjectURL(blob);
-             link.textContent = 'Download Words Starting with "b"';
-        } else {
-             results.innerHTML="<mark>No word with letter ' b '</mark>";
-        }
-     });
+//              filteredArr.push(...filteredwords);
+//              console.log(filteredArr);
+//              let longestWord = "";
+//              for (const word of filteredArr) {
+//                  if (word.length > longestWord.length) {
+//                      longestWord = word;
+//                      results.innerHTML=`<strong>The longest word with letter 'b':</strong>  <mark> ${longestWord}</mark> `;
+//                  }
+//              }
+//              // blob for browsed file
+//              const blob = new Blob([filteredArr.join("\n")], { type: "text/plain" });
+//              console.log(blob);
+//              const fileDownload = new File([blob], "words.txt", { type: "text/plain" });
+//              link.href = URL.createObjectURL(blob);
+//              link.textContent = 'Download Words Starting with "b"';
+//         } else {
+//              results.innerHTML="<mark>No word with letter ' b '</mark>";
+//         }
+//      });
 
-     reader.readAsText(file);
- });
+//      reader.readAsText(file);
+//  });
   // blob for downloaded file
   const Dblob = new Blob(
                  [
@@ -41,6 +41,7 @@
                  ],
                  { type: "text/plain" }
              );
+             downloadedwords.href=URL.createObjectURL(Dblob);
            // resettin
  const resetButton = document.getElementById("reset");
 
